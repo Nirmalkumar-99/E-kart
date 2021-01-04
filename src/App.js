723f3757951f84bd75c2bcaec5abc9b079fb7bb3
Nirmalkumar-99 <nirmalkumar.anbalagan@nutechnologyinc.com>
2021-01-04 17:15:13 +0530

    Shop commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import React, { Component, Fragment } from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import ProductList from "./components/ProductList";
-import Details from "./components/Details";
-import Cart from "./components/Cart";
-import Default from "./components/Default";
-import Modal from "./components/Modal";
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import Header from './components/Header'
+import Section from './components/Section'
+import {DataProvider} from './components/Context'
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <header>
-          <Navbar />
-        </header>
-        <main>
-          <Switch>
-            <Route exact path="/" component={ProductList} />
-            <Route exact path="/details" component={Details} />
-            <Route exact path="/cart" component={Cart} />
-            <Route component={Default} />
-          </Switch>
-          <Modal />
-        </main>
-      </Fragment>
+
+class App extends React.Component{
+  render(){
+    return(
+      <DataProvider>
+        <div className="app">
+          <Router>
+            <Header />
+            <Section />
+          </Router>
+        </div>
+      </DataProvider>
     );
   }
 }
